6392030773512598d1327e871af5e3eb01d2b56c
Ivan <reinkarnaci9@gmail.com>
2020-05-29 00:30:14 +0300

    Выполнены и работают пункты с 1-4

--- lesson-6/Animals/src/ru/geekbrains/lesson6/Main.java
+++ lesson-6/Animals/src/ru/geekbrains/lesson6/Main.java
@@ -25,12 +25,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int scanChoice = scanner.nextInt();
         if (scanChoice == 1) {
-            catsList();
-            dogsList();
+            catsInfoList(catsList());
+            dogsInfoList(dogsList());
         } else if (scanChoice == 2) {
-            checkCatSkills();
+       checkCatSkills(catsList());
+
         } else if (scanChoice == 3) {
-            checkDogSkills();
+            checkDogSkills(dogsList());
         } else {
             startApp();
         }
@@ -39,20 +40,14 @@ public class Main {
     public static Cats catsList() {
 
         Cats cat1 = new Cats("Boris", 200, 0, 2);
+        return cat1;
+    }
+
+    public static void catsInfoList (Cats cat1) {
         cat1.catPresent();
         cat1.run();
         cat1.jump();
         cat1.swim();
-        return cat1;
-    }
-
-    public static Dogs dogsList() {
-        Dogs dog1 = new Dogs("Shurik", 500, 10, 0.5);
-        dog1.dogPresent();
-        dog1.run();
-        dog1.jump();
-        dog1.swim();
-        return dog1;
     }
 
     public static void checkCatSkills(Cats cat1) {
@@ -65,17 +60,17 @@ public class Main {
             System.out.println("Input run distance in meters.");
             double newRunDistance = scanner.nextDouble();
             if (newRunDistance > runDistance) {
-                System.out.println("Cat can't run so far.");
+                System.out.println(String.format("Cat can't run so far. Max run distance for cat %s meters.", runDistance));
             } else {
-                System.out.println("Not bad. Max run distance for cat 200 meters.");
+                System.out.println(String.format("Great, its true. Max run distance for cat %s meters.", runDistance));
             }
         } else if (skillNumber == 2) {
             System.out.println("Input jump height in meters.");
-            double newJumpDistance = scanner.nextDouble();
-            if (newJumpDistance > jumpHeight) {
-                System.out.println("Cat can't jump so far.");
+            double newJumpHeight = scanner.nextDouble();
+            if (newJumpHeight > jumpHeight) {
+                System.out.println(String.format("Cat can't jump so far. Max jump height for cat 2 meters.", jumpHeight));
             } else {
-                System.out.println("Not bad. Max jump height for cat 2 meters.");
+                System.out.println(String.format("Great, its true. Max jump height for cat 2 meters.", jumpHeight));
             }
         } else if (skillNumber == 3) {
             System.out.println("Input swim distance in meters");
@@ -87,7 +82,49 @@ public class Main {
             }
         }
     }
+    public static Dogs dogsList() {
+        Dogs dog1 = new Dogs("Shurik", 500, 10, 0.5);
+        return dog1;
+    }
 
-    public static void checkDogSkills() {
+    public static void dogsInfoList (Dogs dog1) {
+        dog1.dogPresent();
+        dog1.run();
+        dog1.jump();
+        dog1.swim();
+    }
+
+    public static void checkDogSkills(Dogs Dog1) {
+        double runDistance = Dog1.getRunDistance();
+        double jumpHeight = Dog1.getJumpHeight();
+        double swimDistance = Dog1.getSwimDistance();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What skills you want check?\n1 - skill to run \n2 - skill to jump \n3 - skill to swim");
+        int skillNumber = scanner.nextInt();
+        if (skillNumber == 1) {
+            System.out.println("Input run distance in meters.");
+            double newRunDistance = scanner.nextDouble();
+            if (newRunDistance > runDistance) {
+                System.out.println(String.format("Dog can't run so far. Max run distance for dog %s meters.", runDistance));
+            } else {
+                System.out.println(String.format("Great, its true. Max run distance for dog %s meters.", runDistance));
+            }
+        } else if (skillNumber == 2) {
+            System.out.println("Input jump height in meters.");
+            double newJumpHeight = scanner.nextDouble();
+            if (newJumpHeight > jumpHeight) {
+                System.out.println(String.format("Dog can't jump so far.Max jump height for dog %s meters.",jumpHeight));
+            } else {
+                System.out.println(String.format("Great, its true. Max jump height for dog %s meters.",jumpHeight));
+            }
+        } else if (skillNumber == 3) {
+            System.out.println("Input swim distance in meters");
+            double newSwimDistance = scanner.nextDouble();
+            if (newSwimDistance > swimDistance) {
+                System.out.println(String.format("Dog can't swim so far.Max swim distance for dog %s meters.", swimDistance));
+            } else {
+                System.out.println(String.format("Great, its true. Max swim distance for dog %s meters.", swimDistance));
+            }
+        }
     }
 }
