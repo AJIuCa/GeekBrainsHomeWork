83001d1aeea6290e2403ba8283d2b46725706947
Ivan <reinkarnaci9@gmail.com>
2020-05-29 12:30:11 +0300

    Выполнены все 5 пунктов домашней работы

--- lesson-6/Animals/src/ru/geekbrains/lesson6/Main.java
+++ lesson-6/Animals/src/ru/geekbrains/lesson6/Main.java
@@ -16,11 +16,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-        startApp();
 
+        startApp();
     }
 
     public static void startApp() {
+
         System.out.println("Please, make choice.\n1 - Animals skill list.\n2 - Check cat skills.\n3 - Check dog skills");
         Scanner scanner = new Scanner(System.in);
         int scanChoice = scanner.nextInt();
@@ -37,60 +38,82 @@ public class Main {
         }
     }
 
-    public static Cats catsList() {
 
-        Cats cat1 = new Cats("Boris", 200, 0, 2);
-        return cat1;
+    public static Cats[] catsList() {
+
+        Cats[] cats = new Cats[3];
+        cats[0] = new Cats("Boris", 200, 0, 2);
+        cats[1] = new Cats("Alice", 500, 0, 2.5);
+        cats[2] = new Cats("Polina", 370, 0, 3);
+        return cats;
     }
 
-    public static void catsInfoList(Cats cat1) {
-        cat1.catPresent();
-        cat1.run();
-        cat1.jump();
-        cat1.swim();
+    public static void catsInfoList(Cats cats[]) {
+
+        for (int i = 0; i < cats.length; i++) {
+            cats[i].catPresent();
+            cats[i].run();
+            cats[i].jump();
+            cats[i].swim();
+        }
     }
 
-    public static void checkCatSkills(Cats cat1) {
-        double runDistance = cat1.getRunDistance();
-        double jumpHeight = cat1.getJumpHeight();
+    public static void checkCatSkills(Cats cats[]) {
+
+        int i;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What skills you want check?\n1 - skill to run \n2 - skill to jump \n3 - skill to swim");
-        int skillNumber = scanner.nextInt();
-        if (skillNumber == 1) {
-            System.out.println("Input run distance in meters.");
-            double newRunDistance = scanner.nextDouble();
-            if (newRunDistance > runDistance) {
-                System.out.println(String.format("Cat can't run so far. Max run distance for cat %s meters.", runDistance));
-            } else {
-                System.out.println(String.format("Great, its true. Max run distance for cat %s meters.", runDistance));
-            }
-        } else if (skillNumber == 2) {
-            System.out.println("Input jump height in meters.");
-            double newJumpHeight = scanner.nextDouble();
-            if (newJumpHeight > jumpHeight) {
-                System.out.println(String.format("Cat can't jump so far. Max jump height for cat 2 meters.", jumpHeight));
-            } else {
-                System.out.println(String.format("Great, its true. Max jump height for cat 2 meters.", jumpHeight));
-            }
-        } else if (skillNumber == 3) {
-            System.out.println("Input swim distance in meters");
-            double newSwimDistance = scanner.nextDouble();
-            if (newSwimDistance > 0) {
-                System.out.println("Cat afraid water. He won't do it.");
-            } else {
-                System.out.println("Cat afraid water. He won't do it.");
-            }
-        } else {
+        System.out.println(String.format("Please, choose a cat.\n1 - Cat %s.\n2 - Cat %s.\n3 - Cat %s.",
+                cats[0].getName(), cats[1].getName(), cats[2].getName()));
+        int scanChooseCat = scanner.nextInt() - 1;
+        if (scanChooseCat > 3) {
             checkCatSkills(catsList());
+        } else if (scanChooseCat < 0) {
+            checkCatSkills(catsList());
+        } else {
+            i = scanChooseCat;
+            double runDistance = cats[i].getRunDistance();
+            double jumpHeight = cats[i].getJumpHeight();
+            double swimDistance = cats[i].getSwimDistance();
+            System.out.println("What skills you want check?\n1 - skill to run \n2 - skill to jump \n3 - skill to swim");
+            int skillNumber = scanner.nextInt();
+            if (skillNumber == 1) {
+                System.out.println("Input run distance in meters.");
+                double newRunDistance = scanner.nextDouble();
+                if (newRunDistance > runDistance) {
+                    System.out.println(String.format("Cat can't run so far. Max run distance for cat %s meters.", runDistance));
+                } else {
+                    System.out.println(String.format("Great, its true. Max run distance for cat %s meters.", runDistance));
+                }
+            } else if (skillNumber == 2) {
+                System.out.println("Input jump height in meters.");
+                double newJumpHeight = scanner.nextDouble();
+                if (newJumpHeight > jumpHeight) {
+                    System.out.println(String.format("Cat can't jump so far. Max jump height for cat %s meters.", jumpHeight));
+                } else {
+                    System.out.println(String.format("Great, its true. Max jump height for cat %s meters.", jumpHeight));
+                }
+            } else if (skillNumber == 3) {
+                System.out.println("Input swim distance in meters");
+                double newSwimDistance = scanner.nextDouble();
+                if (newSwimDistance > 0) {
+                    System.out.println("Cat afraid water. He won't do it.");
+                } else {
+                    System.out.println("Cat afraid water. He won't do it.");
+                }
+            } else {
+                checkCatSkills(catsList());
+            }
         }
     }
 
     public static Dogs dogsList() {
+
         Dogs dog1 = new Dogs("Shurik", 500, 10, 0.5);
         return dog1;
     }
 
     public static void dogsInfoList(Dogs dog1) {
+
         dog1.dogPresent();
         dog1.run();
         dog1.jump();
@@ -98,6 +121,7 @@ public class Main {
     }
 
     public static void checkDogSkills(Dogs Dog1) {
+
         double runDistance = Dog1.getRunDistance();
         double jumpHeight = Dog1.getJumpHeight();
         double swimDistance = Dog1.getSwimDistance();
