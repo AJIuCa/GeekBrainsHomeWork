298e236b096550c96c7c1e0f60f3616d942e1042
Ivan <reinkarnaci9@gmail.com>
2020-06-30 21:36:36 +0300

    Выполнено

--- CmdMessageApp/src/ru/geekbrains/lesson_2_6/Client.java
+++ CmdMessageApp/src/ru/geekbrains/lesson_2_6/Client.java
@@ -9,21 +9,50 @@ public class Client {
     private final int SERVER_PORT = 503;
     private DataInputStream in;
     private DataOutputStream out;
+    private DataInputStream accept;
+    private DataOutputStream send;
     private BufferedReader text;
+    private Socket socket;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         new Client().runClient();
     }
 
-    public void runClient() {
+    public void runClient() throws IOException {
 
         try {
-            Socket socket = new Socket(SERVER_Address, SERVER_PORT);
+            socket = new Socket(SERVER_Address, SERVER_PORT);
             text = new BufferedReader(new InputStreamReader(System.in));
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
             System.out.println("Client connecting to server");
+
+            Thread outcome = new Thread(()-> {
+                try {
+                    sendMsg();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            Thread income = new Thread(()-> {
+                try {
+                    acceptMsg();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            outcome.start();
+            income.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+     public void sendMsg() throws IOException {
 
             while (!socket.isOutputShutdown()) {
                 if (text.ready()) {
@@ -39,8 +68,21 @@ public class Client {
                     System.out.println("Client take message from server = " + message);
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+         }
+     public void acceptMsg () throws IOException {
+             accept = new DataInputStream(socket.getInputStream());
+             send = new DataOutputStream(socket.getOutputStream());
+
+             while (true) {
+                 String message = accept.readUTF();
+                 System.out.println("Server take message from client = " + message);
+                 if (message.equals("/disconnect")) {
+                     System.out.println("Client disconnect");
+                     break;
+                 }
+                 System.out.println("Server Wrote message to client.");
+                 send.writeUTF(message);
+
+             }
+         }
     }
-}
--- CmdMessageApp/src/ru/geekbrains/lesson_2_6/Server.java
+++ CmdMessageApp/src/ru/geekbrains/lesson_2_6/Server.java
@@ -1,40 +1,50 @@
 package ru.geekbrains.lesson_2_6;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private DataInputStream acceptMsg;
+
+    private BufferedReader msg;
+    private DataOutputStream sendServerMsg;
+
+
     public static void main(String[] args) {
 
         new Server().runServer();
+
     }
 
     public void runServer() {
 
         try (ServerSocket serverSocket = new ServerSocket(503)) {
             System.out.println("\n-= Starting server (local port = " + serverSocket.getLocalPort() + "). Waiting for connection =-");
-            Socket socket = serverSocket.accept();
+            socket = serverSocket.accept();
             System.out.println(" -= Client connected =-");
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            while (true) {
-                String message = in.readUTF();
-                System.out.println("Server take message from client = " + message);
-                if (message.equals("/disconnect")) {
-                    System.out.println("Client disconnect");
-                    break;
-                }
-                System.out.println("Server Wrote message to client.");
-                out.writeUTF(message);
-            }
 
-            in.close();
-            out.close();
-            socket.close();
+            Thread thread1 = new Thread(() -> {
+                try {
+                    acceptMsg();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            Thread thread2 = new Thread(() -> {
+                try {
+                    sendMsg();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            thread1.start();
+            thread2.start();
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -42,5 +52,45 @@ public class Server {
     }
 
 
+    public void acceptMsg() throws IOException {
+
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+
+        while (true) {
+            String message = in.readUTF();
+            System.out.println("Server take message from client = " + message);
+            if (message.equals("/disconnect")) {
+                System.out.println("Client disconnect");
+                break;
+            }
+            System.out.println("Server Wrote message to client.");
+            out.writeUTF(message);
+
+        }
+    }
+
+
+    public void sendMsg() throws IOException {
+
+        acceptMsg = new DataInputStream(socket.getInputStream());
+        sendServerMsg = new DataOutputStream(socket.getOutputStream());
+        msg = new BufferedReader(new InputStreamReader(System.in));
+
+        while (!socket.isOutputShutdown()) {
+            if (msg.ready()) {
+                String textToClient = msg.readLine();
+                sendServerMsg.writeUTF(textToClient);
+                System.out.println("Server send msg = " + textToClient);
+                if (textToClient.equals("/disconnect")) {
+                    System.out.println("Connection lost");
+                    break;
+                }
+                System.out.println("Server send msg to client");
+                String message = acceptMsg.readUTF();
+                System.out.println("Server take message from server = " + message);
+            }
+        }
+    }
 }
 
