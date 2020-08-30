df5e5f293b234f5b6154a751ad6c0707d78fd127
Ivan <reinkarnaci9@gmail.com>
2020-08-30 10:42:10 +0300

    Задачи 1 и 2 выполнены

---
+++ generics/src/ru/geekbrains/lesson_3_1/Apple.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.lesson_3_1;
+
+public class Apple extends Fruit {
+
+    public Apple(float fruitWeight) {
+        super(1.0f);
+    }
+}
---
+++ generics/src/ru/geekbrains/lesson_3_1/Orange.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.lesson_3_1;
+
+public class Orange extends Fruit {
+
+    public Orange(long fruitWeight) {
+        super(1.5f);
+    }
+}
